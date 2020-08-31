b9a50122f845f38d591985c26d0e89b3b1aa8ba9
olivier delfosse <olividelfosse@gmail.com>
2020-08-31 17:40:16 +0200

    add tournaments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require_relative "./seed_sam"
 
 Message.destroy_all
 puts "DESTROYED : Message"
@@ -178,3 +172,6 @@ TournamentParticipation.create!(
 #     compteur += 1
 #   end
 # end
+
+
+# seed_prod()
