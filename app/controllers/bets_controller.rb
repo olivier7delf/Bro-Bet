6f1588cdda3b1099a52a84c63dbd2ca7cf83b027
Theotime Lameyre <theotime.lameyre@gmail.com>
2020-09-03 15:24:06 +0200

    Petits changements

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,5 +1,14 @@
 class BetsController < ApplicationController
   before_action :set_bet, only: [:show]
+  def index
+    @user = current_user
+
+    @finished_bets = @user.in_bets.where.not(result: nil)
+    @finished_bets.order(resulted_at: :desc)
+
+    @pending_bets = @user.in_bets.where(result: nil)
+    @pending_bets.order(resulted_at: :desc)
+  end
 
   def show
     # @bet = Bet.find(params[:id])
