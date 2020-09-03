6ebf9b2118d0275308bd38d61f145efd42699011
Theotime Lameyre <theotime.lameyre@gmail.com>
2020-09-03 20:36:04 +0200

    nouveau seed

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -3,11 +3,9 @@ class BetsController < ApplicationController
   def index
     @user = current_user
 
-    @finished_bets = @user.in_bets.where.not(result: nil)
-    @finished_bets.order(resulted_at: :desc)
+    @finished_bets = @user.in_bets.where.not(result: nil).order(resulted_at: :asc)
 
-    @pending_bets = @user.in_bets.where(result: nil)
-    @pending_bets.order(resulted_at: :desc)
+    @pending_bets = @user.in_bets.where(result: nil).order(resulted_at: :asc)
   end
 
   def show
