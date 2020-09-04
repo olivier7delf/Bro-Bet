ea292141c7a18a8c32d01a19fcce6751ab80e74d
Theotime Lameyre <theotime.lameyre@gmail.com>
2020-09-04 11:44:19 +0200

    petit bug fixés

--- app/models/bet_participation.rb
+++ app/models/bet_participation.rb
@@ -3,4 +3,18 @@ class BetParticipation < ApplicationRecord
   belongs_to :bet
 
   # validates :user_choice, presence: true
+  def self.get_user_choice(bet, user)
+    bet_participation = BetParticipation.find_by(bet: bet, user: user)
+    if bet_participation
+      if bet_participation.user_choice
+        "BRO"
+      else
+        "NO BRO"
+      end
+    elsif bet.closed_at < DateTime.now
+      "Trop tard"
+    else
+      "En attente"
+    end
+  end
 end
