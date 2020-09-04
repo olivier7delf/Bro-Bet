1d21fee0768ac6e69db048cc45c69c74e32d513c
olivier delfosse <olividelfosse@gmail.com>
2020-09-04 11:45:34 +0200

    bonus see in participant choices

--- app/models/bonus_progress.rb
+++ app/models/bonus_progress.rb
@@ -5,18 +5,19 @@ class BonusProgress < ApplicationRecord
   belongs_to :tournament
 
   def self.calcul_available_bonuses(score, score_best_player)
-    bonuses = Bonuse.all.each { |bonus| bonus.probability = 10 * bonus.probability * (1.5 - score / (score_best_player + 1)) }
+    bonuses = Bonuse.all.each { |bonus| bonus.probability = 2 * bonus.probability * (1.5 - score / (score_best_player + 1)) }
     available_bonuses = bonuses.select { |bonus| bonus.probability * 100 > Random.rand(1..100) }
-
   end
 
   def self.get_available_bonuses(user, bet, tournament)
-    # raise
     BonusProgress.includes(:bonuse).where(user: user, bet: bet, tournament: tournament, progress: "available")
   end
 
   def self.get_used_bonuse(user, bet, tournament)
-
     BonusProgress.includes(:bonuse).find_by(user: user, bet: bet, tournament: tournament, progress: "used")
+  end
+
+  def self.get_used_bonuses(bet, tournament)
+    BonusProgress.includes(:bonuse).where(bet: bet, tournament: tournament, progress: "used")
   end
 end
